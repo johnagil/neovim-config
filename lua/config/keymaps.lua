12150fff6bf2dc3811954c19a2a270c93affd0d1
Johnagil <34685272+johnagil@users.noreply.github.com>
2025-10-07 10:41:56 -0500

    changed resize window bindings to use alt instead of control

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,10 @@ vim.cmd("nmap <leader>c :e ~/.config/nvim/init.lua<cr>")
 
 -- redo with capital U
 vim.keymap.set("n", "U", "<C-r>")
+
+-- change resize windows bindings to alt + arrow keys
+-- because macOS mission control intercepts control + arrow keys
+vim.keymap.set("n", "<A-Up>", ":resize +2<CR>", { silent = true })
+vim.keymap.set("n", "<A-Down>", ":resize -2<CR>", { silent = true })
+vim.keymap.set("n", "<A-Left>", ":vertical resize -2<CR>", { silent = true })
+vim.keymap.set("n", "<A-Right>", ":vertical resize +2<CR>", { silent = true })
