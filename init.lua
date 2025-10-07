d77aca89b7f87e25faa1315696880927511485e0
johnagil <34685272+johnagil@users.noreply.github.com>
2025-10-06 21:26:25 -0500

    moved bindings to keymaps.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- <leader> key (space bar)
-vim.g.mapleader = " "
-
 -- use standard system clipboard when copying and pasting
 -- vim.opt.clipboard:append("unnamedplus")
 vim.opt.clipboard = vim.opt.clipboard + { "unnamedplus" }
@@ -13,9 +10,3 @@ vim.opt.ignorecase = true
 
 -- if uppercase is used in search it becomes case sensitive
 vim.opt.smartcase = true
-
--- open config with leader c
-vim.cmd("nmap <leader>c :e ~/.config/nvim/init.lua<cr>")
-
--- redo with capital U
-vim.keymap.set("n", "U", "<C-r>")
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- <leader> key (space bar)
+vim.g.mapleader = " "
+
+-- open config with leader c
+vim.cmd("nmap <leader>c :e ~/.config/nvim/init.lua<cr>")
+
+-- redo with capital U
+vim.keymap.set("n", "U", "<C-r>")
